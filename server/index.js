7e79cfe6424f1398bd52d74afc2834a5d173aa71
Genne Liu <genneliu@gmail.com>
2021-09-23 13:07:07 -0700

    basic files

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const path = require("path")
+
+const app = express()
+
+app.get('/', (req, res) => { 
+    res.sendFile(path.join(__dirname, "../index.html"))
+})
+
+//environmental variables
+//env sensitive information not shared with github
+const port = process.env.PORT || 4400
+
+app.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+})
