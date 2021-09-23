b1cb31de55435b798d6c4a04bc9529be4123b7a7
Genne Liu <genneliu@gmail.com>
2021-09-23 13:11:23 -0700

    express static

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
-const express = require("express")
+const express = require("express");
 
-const path = require("path")
+const path = require("path");
 
-const app = express()
+const app = express();
+
+app.use(express.static("client"));
 
 app.get('/', (req, res) => { 
     res.sendFile(path.join(__dirname, "../index.html"))
