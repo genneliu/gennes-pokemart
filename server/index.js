f528084a320e4289da5ad30789bcc3e660ea052e
Genne Liu <genneliu@gmail.com>
2021-09-23 15:18:06 -0700

    IMAGGES

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.static("client"));
-app.use(express.static('client/assets'));
+
 
 app.get('/', (req, res) => { 
     res.sendFile(path.join(__dirname, "../index.html"))
