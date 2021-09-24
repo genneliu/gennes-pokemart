621a940dfafe3eb05035a94bb7e5d368b9751bad
Genne Liu <genneliu@gmail.com>
2021-09-23 18:12:35 -0700

    button and header

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
 
 
-secretButton = document.getElementById("secret")
+// secretButton = document.getElementById("secret")
 
-function displaySecretItem () {
-    let revealItem = document.createElement("p")
-    revealItem.textContent = "Hello" //find a way to append image?
-    secretButton.appendChild(revealItem)
-}
+// function displaySecretItem () {
+//     let revealItem = document.createElement("p")
+//     revealItem.textContent = "Hello" //find a way to append image?
+//     secretButton.appendChild(revealItem)
+// }
 
-secretButton.addEventListener('click', displaySecretItem)
+// secretButton.addEventListener('click', displaySecretItem)
